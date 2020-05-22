764d799a407d1cca6bf9cc6a2016de53cc321835
Łukasz Mojek <lukasz.mojek@gmail.com>
2020-05-22 08:21:12 +0100

    Initial program flow created

--- TouristEquipmentRental/TouristEquipmentRental.cpp
+++ TouristEquipmentRental/TouristEquipmentRental.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include "FileDatabase.h"
+#include "DatabaseOperator.h"
 
 using namespace std;
 
 int main()
 {
     auto fileDatabase = new FileDatabase();
+
+    auto databaseOperator = new DatabaseOperator(* fileDatabase);
+
+    try
+    {
+        auto user = databaseOperator->GetUser("ratatata@gmail.com", "kowalstwo");
+
+        auto reservations = databaseOperator->GetUserReservations("U420");
+
+        for (auto reservation : reservations)
+        {
+            auto reservationEquipment = databaseOperator->GetReservationEquipment(reservation.Id());
+            reservation.AssignEquipmentToReservation(reservationEquipment);
+            user.AssignReservationToUser(reservation);
+        }
+    }
+    catch (exception e)
+    {
+    
+    }
+    
+
     return 0;
 }
